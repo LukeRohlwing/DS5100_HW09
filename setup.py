11d0170faeb650a24d17e78ff22c81a02085bb21
LukeRohlwing <77292961+LukeRohlwing@users.noreply.github.com>
2023-07-31 12:09:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='booklover',
     version='1.0.0',
-    url='https://github.com/LukeRohlwing/DS5100_HW09',
+    url='https://github.com/LukeRohlwing/DS5100_hw09',
     author='Luke Rohlwing',
     author_email='sqz4dh@virginia.edu',
     description='Demo package',
